de606fb9d65d2604e62bc1e041b6702d80d0c194
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-13 19:08:26 -0500

    add sliding nav bar

--- portfolio.js
+++ portfolio.js
@@ -2,10 +2,14 @@ const navIcon = document.querySelector(".nav-icon-close");
 
 function openNav() {
     navIcon.classList.add("nav-icon-open")
+    document.getElementById("mySidenav").style.width = "75%";
+    navIcon.classList.add("color-change");
 };
 
 function closeNav() {
     navIcon.classList.remove("nav-icon-open")
+    document.getElementById("mySidenav").style.width = "0";
+    navIcon.classList.remove("color-change");
 };
 
 navIcon.addEventListener("click", function () {
